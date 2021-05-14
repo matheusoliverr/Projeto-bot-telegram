d194b6a14e2db8a7db84ce442dd185924725a64a
Matheus de Oliveira <71405941+matheusoliverr@users.noreply.github.com>
2021-05-14 20:52:26 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const { Pool } = require("pg")
 
 module.exports = new Pool({
-    user: "postgres",
-    password: "sql1208",
+    user: "",
+    password: "",
     host: "localhost",
     port: 5432,
     database: "js_code_maker"
